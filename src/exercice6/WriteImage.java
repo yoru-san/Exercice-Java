4c8c10cbc827ecf213e6677545cbfaba7cfc44ad
MissYoruwara <cecel31@free.fr>
2017-10-15 16:17:14 +0200

    exercice 6

--- src/exercice6/WriteImage.java
+++ src/exercice6/WriteImage.java
@@ -1,5 +1,35 @@
 package exercice6;
 
-public class WriteImage {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 
+public class WriteImage {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        String imageFormat = readEntry("image format");
+        String path = readEntry("path");
+        String link = "https://";
+        link += readEntry("url without 'https://'");
+        File imageFile = new File(path);
+        BufferedImage image;
+        try {
+
+            URL url = new URL(link);
+            image = ImageIO.read(url);
+
+            ImageIO.write(image, imageFormat , imageFile);
+
+        } catch (IOException e) {
+            System.out.println("Raised exception : " + e.getMessage());
+        }
+    }
+
+    public static String readEntry(String entry) {
+        System.out.println("Enter the " + entry);
+        return sc.next();
+    }
 }
